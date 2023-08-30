2d78038a456c66fe8d25694db2561b16e023faff
EnchantPlus <134760586+EnchantPlus@users.noreply.github.com>
2023-08-30 10:24:36 +0530

    Update AutoTool.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/AutoTool.kt
@@ -1,29 +1,15 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/SkidderMC/FDPClient/
- */
 package net.ccbluex.liquidbounce.features.module.modules.player
 
-import net.ccbluex.liquidbounce.FDPClient
 import net.ccbluex.liquidbounce.event.ClickBlockEvent
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.features.value.BoolValue
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
-import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.minecraft.network.play.client.C09PacketHeldItemChange
 import net.minecraft.util.BlockPos
 
-@ModuleInfo(name = "AutoTool", category = ModuleCategory.PLAYER)
-object AutoTool : Module() {
-
-    private val noCombat = BoolValue("NoCombat", true)
-    private val silent = BoolValue("Silent", false)
+object AutoTool : Module("AutoTool", ModuleCategory.PLAYER) {
 
     @EventTarget
     fun onClick(event: ClickBlockEvent) {
-        if (FDPClient.combatManager.inCombat && noCombat.get()) return
         switchSlot(event.clickedBlock ?: return)
     }
 
@@ -31,11 +17,11 @@ object AutoTool : Module() {
         var bestSpeed = 1F
         var bestSlot = -1
 
-        val block = mc.theWorld.getBlockState(blockPos).block
+        val blockState = mc.theWorld.getBlockState(blockPos)
 
         for (i in 0..8) {
             val item = mc.thePlayer.inventory.getStackInSlot(i) ?: continue
-            val speed = item.getStrVsBlock(block)
+            val speed = item.getStrVsBlock(blockState.block)
 
             if (speed > bestSpeed) {
                 bestSpeed = speed
@@ -43,13 +29,8 @@ object AutoTool : Module() {
             }
         }
 
-            if (bestSlot != -1) {
-                if (!silent.get()) {
-                    mc.thePlayer.inventory.currentItem = bestSlot
-                } else {
-                    mc.netHandler.addToSendQueue(C09PacketHeldItemChange(bestSlot))
-                    mc.playerController.updateController()             
-        }
+        if (bestSlot != -1)
+            mc.thePlayer.inventory.currentItem = bestSlot
     }
-  }
+
 }
